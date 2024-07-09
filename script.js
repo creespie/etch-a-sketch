9df2c35b73a177d8f60760f66771ad74734c5a4f
creespie <150524904+creespie@users.noreply.github.com>
2024-07-09 14:04:32 +0200

    add color change and input for grid length

--- script.js
+++ script.js
@@ -1,14 +1,61 @@
 function creator(number){
-    let total = number * number;
+    //check if number is below 100
     
-    while (total > 0){
+    let childNumber = number;
+    
+    
+    //remove elements
+
+    let count = document.getElementById("container").children.length;
+
+    if(count >0){
         const children = document.createElement("div");
         const container = document.getElementsByClassName("container")[0];
+        const rowChildren = document.createElement("div");
+        children.removeChild(rowChildren);
+        container.removeChild(children);
+        count--;
+    };
+
+    if(number <= 100){
+    //creates elements
+    while (childNumber > 0){
+        const children = document.createElement("div");
+        const container = document.getElementsByClassName("container")[0];
+        let rowNumber = number;
         children.classList.add("children");
         container.appendChild(children);
-        total--;
+        while(rowNumber > 0){
+            const rowChildren = document.createElement("div");
+            rowChildren.classList.add("rowChildren");
+            children.appendChild(rowChildren);
+            rowNumber--;
+        }
+        childNumber--;
     }
+    }
+
+    //change colors 
+    const singleSquares = document.querySelectorAll(".rowChildren");
+    singleSquares.forEach((singlesquares) => {
+    
+        singlesquares.addEventListener("mouseenter", (e) => {
+            singlesquares.style.backgroundColor = "red";
+        });
+    });
 }
 
-creator(2);
+
+
+
+const button = document.getElementById("confirm");
+button.addEventListener("click", () => {
+    let value = document.getElementById("number").value;
+    creator(value);
+  });
+
+
+
+
+
 
